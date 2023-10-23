acfed3b711b6a7f2c26a29da86c9c157674c3344
ostifede02 <ostifederico02@gmail.com>
2023-10-23 16:52:08 -0400

    backup

--- script/trajectory_planning/path_planning.py
+++ script/trajectory_planning/path_planning.py
@@ -1,25 +1,28 @@
 import numpy as np
 
+'''
+ __     __         _       _     _           
+ \ \   / /_ _ _ __(_) __ _| |__ | | ___  ___ 
+  \ \ / / _` | '__| |/ _` | '_ \| |/ _ \/ __|
+   \ V / (_| | |  | | (_| | |_) | |  __/\__ \
+    \_/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/
+                                             
+'''
 
-z_offset = 40
+z_offset = 60
 # start and end point
-P0 = np.array([-150, 0, -280])
-P3 = np.array([0, 0, -280])
+P0 = np.array([-200, 0, -280])
+P3 = np.array([200, 0, -280])
 # via points
 P1 = np.array([P0[0], 0, P0[2]+z_offset])
 P2 = np.array([P3[0], 0, P3[2]+z_offset])
 
 
 # max acceleration and velocity
-max_acc = 1
-max_vel = 150
-s_vel = 0
-
-# time increment in seconds
-delta_t = 0.01
-
-# overall time taken
-T = 0
+max_acc = 120        # [ mm / s2 ]
+max_vel = 150      # [ mm / s ]
+next_vel = 0
+delta_t = 0
 
 # states time scaling
 STATE_ACC = "state const acc"
@@ -31,76 +34,82 @@ state = STATE_ACC   # initial state
 # state change flags
 # from acceleration to constant velocity OR deceleration
 s_1 = 0
-t_1 = 0
-# from constant velocity to deceleration
-s_2 = 0
-t_2 = 0
 
+
+
+
+'''
+  _____                 _   _                 
+ |  ___|   _ _ __   ___| |_(_) ___  _ __  ___ 
+ | |_ | | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+ |  _|| |_| | | | | (__| |_| | (_) | | | \__ \
+ |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+                                              
+'''
 
 
 # in: s -> [0, 1]   out: X
 def bezier_curve(s):
+    s = max(s, 0)
+    s = min(s, 1)
     x_next = pow(1-s, 3)*P0 + 3*pow(1-s, 2)*s*P1 + 3*(1-s)*pow(s, 2)*P2 + pow(s, 3)*P3
-    # x_next = P0 + s*(P3-P0)
+    # x_next = (1-s)*P0 + s*P3
     return x_next
 
-# calculate the distance between two consecutive points divided by the time step
-def current_velocity(t):
-    s_current = (max_acc / 2) * pow(t, 2)
-    s_next = (max_acc / 2) * pow((t+delta_t), 2)
-    vel = np.linalg.norm((bezier_curve(s_current) - bezier_curve(s_next)) / delta_t)
-    return vel
+def get_roots(a, b, c, sol):
+    if sol == 1:
+        return (-b + np.sqrt(abs(pow(b,2)-4*a*c)))/(2*a)
+    elif sol == 2:
+        return (-b - np.sqrt(abs(pow(b,2)-4*a*c)))/(2*a)
+    return -1
 
-def euclidean_distance(s, s_next):
-    delta_x = np.linalg.norm(bezier_curve(s_next) - bezier_curve(s))
-    return delta_x
+# in: t -> [0, T]   out: time_taken
+def time_taken_interval(s, delta_s):
+    global state, s_1, next_vel
 
-# in: t -> [0, T]   out: s -> [0, 1]
-def time_scaling(t):
-    delta_time_correction = 0
-    global state, t_1, s_1, t_2, s_2, T, s_vel
-
-    if state is None:
+    if s >= 1:
         state = STATE_ACC
-        # t_1, s_1, t_2, s_2, T = 0
-        return None, 0
+        return None
+     
+    x_current = bezier_curve(s)
+    x_next = bezier_curve(s+delta_s)
+    delta_x = np.linalg.norm(x_next - x_current)
+    prev_vel = next_vel
 
     # acceleration
-    if state == STATE_ACC:
-        s = (max_acc / 2) * pow(t, 2)
-
-        if s > 0.5:
+    if state == STATE_ACC:   
+        if s >= 0.5:
             state = STATE_DEC
-            T = 2*t
-            t_1 = t_2 = t
-            s_1 = s_2 = s
-        elif current_velocity(t) >= max_vel:
+        
+        # x0 + v0*t + 0.5*a0*t^2 = x
+        # 0.5*a0*delta_t^2 + v0*delta_t - delta_x = 0
+        # a = 0.5*a0;   b = v0;  c = -delta_x
+        delta_t = get_roots(0.5*max_acc, prev_vel, -delta_x, sol=1)
+        # v1 = v0 + a0*t
+        next_vel = prev_vel + max_acc * delta_t
+
+        if next_vel >= max_vel:
             state = STATE_VEL
-            s_vel = max_acc*t
-            t_1 = t
-            s_1 = s
+            next_vel = prev_vel
+            s_1 = s 
+            
 
     # constant velocity
-    if state == STATE_VEL:
-        s = s_vel * (t - t_1) + s_1
-        s_prev = s_vel * (t - delta_t - t_1) + s_1
-
-        delta_time_correction = (euclidean_distance(s, s_prev) / max_vel) - delta_t
-        # delta_time_correction = 0
+    if state == STATE_VEL:        
+        # v = dx/dt -> dt = dx/v
+        delta_t = delta_x / prev_vel
 
         if s >= (1 - s_1):
             state = STATE_DEC
-            T = t_1 + t
-            t_2 = t
-            s_2 = s
+
 
     # deceleration
     if state == STATE_DEC:
-        s = s_1 - abs((max_acc / 2) * pow((t - (t_2 + t_1)), 2)) + s_2
+        # x0 + v0*t + 0.5*a0*t^2 = x
+        # -0.5*a0*delta_t^2 + v0*delta_t - delta_x = 0
+        # a = -0.5*a0;   b = v0;  c = -delta_x
+        delta_t = get_roots(-0.5*max_acc, prev_vel, -delta_x, sol=1)
+        # v1 = v0 + a0*t
+        next_vel = prev_vel - max_acc * delta_t
 
-        if s >= 1:
-            state = None
-            s = 1
-            print("s=",s)
-
-    return s, delta_time_correction
+    return delta_t
--- script/trajectory_planning/path_test.py
+++ script/trajectory_planning/path_test.py
@@ -1,76 +1,91 @@
-import path_planning as pp
-from path_planning import *
+import path_planning_V2 as pp
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 
-t = 0
 s = 0
+delta_s = 0.01      # time increment in seconds
 s_index = 0
 s_iteration_filter = 1
+
+delta_t = 0
+T = 0
 
 N = 100000
 time_scaling_plot_data = np.empty((2,N))
 x_des_plot_data = np.empty((2, N))
 vel_plot_data = np.empty(N)
+delta_t_plot_data = np.empty(N)
 vel = 0
 plot_index = 0
 
-x_des = bezier_curve(0)
-x_prev = np.empty(3)
-
 while True:
-    x_prev = x_des
+    # **********  START of code  ************
+    delta_t = pp.time_taken_interval(s, delta_s)
+    x_des = pp.bezier_curve(s)
+    delta_x = np.linalg.norm(pp.bezier_curve(s+delta_s) - x_des)
 
-    s, delta_time_correction = time_scaling(t)
+    if delta_t is None:
+        vel_plot_data[plot_index-1] = vel
 
-    if s is None:
+        x_des = pp.bezier_curve(1)
+        x_des_plot_data[:, plot_index] = np.array([x_des[0], x_des[2]])
         break
+    
+    vel = delta_x / delta_t
+    T += delta_t
+    s = round(s + delta_s, 3)
+    
+    # **********  END of code  ************
 
-    if s_index % s_iteration_filter != 0:
-        s_index += 1
-        t = round(t + delta_t, 3)
-        continue
-    else:
-        s_index += 1
-        t = round(t + delta_t, 3)
-
-    x_des = bezier_curve(s)
+    # **********  save data for plots  ************
 
     # save data for plotime_scaling_plot_data
-    time_scaling_plot_data[:, plot_index]   = np.array([t, s])
+    # time_scaling_plot_data[:, plot_index]   = np.array([s, s]) -> WARNING s,s
     x_des_plot_data[:, plot_index]          = np.array([x_des[0], x_des[2]])
-
-    vel = np.linalg.norm(x_des-x_prev) / (pp.delta_t + delta_time_correction)
+    
+    # vel = pp.get_velocity_bezier_curve(s, delta_s, delta_t)
     vel_plot_data[plot_index] = vel
+    delta_t_plot_data[plot_index] = delta_t
+
     plot_index += 1
 
-print(f"t1={pp.t_1}\tt2={pp.T-pp.t_2}\tT={pp.T}")
-print(f"e={np.linalg.norm(x_des-bezier_curve(1))}")
+print(f"t1={pp.s_1}\tT={T}\ts={s}")
+print(f"e={np.linalg.norm(x_des-pp.bezier_curve(1))}")
 
 
 
 
 # PLOT STUFF
-plot_time_scale_fn = plt.figure("position time scaling profile")
-plot_path = plt.axis("equal")
-plot_path = plt.hlines(0, 0, pp.T, 'k', '--')
-plot_path = plt.hlines(1, 0, pp.T, 'k', '--')
-plot_path = plt.hlines(pp.s_1, 0, pp.T, 'r', '--')
-plot_path = plt.hlines(1-pp.s_1, 0, pp.T, 'r', '--')
-plot_path = plt.plot(time_scaling_plot_data[0, 0:plot_index], time_scaling_plot_data[1, 0:plot_index])
+# plot_time_scale_fn = plt.figure("position time scaling profile")
+# plot_path = plt.axis("equal")
+# plot_path = plt.hlines(0, 0, 1, 'k', '--')
+# plot_path = plt.hlines(1, 0, 1, 'k', '--')
+# plot_path = plt.hlines(pp.s_1, 0, s, 'r', '--')
+# plot_path = plt.hlines(1-pp.s_1, 0, s, 'r', '--')
+# plot_path = plt.plot(time_scaling_plot_data[0, 0:plot_index], time_scaling_plot_data[1, 0:plot_index])
 
 # position with continuos position
 plot_path = plt.figure("end effector trajectorty")
 plot_path = plt.axis("equal")
-plot_path = plt.plot(x_des_plot_data[0, 0:plot_index], x_des_plot_data[1, 0:plot_index], '.',label="continuous position")
+plot_path = plt.plot(x_des_plot_data[0, 0:plot_index+1], x_des_plot_data[1, 0:plot_index+1], '.',label="continuous position")
 
-# position with continuos position
+# velocity profile
 plot_vel = plt.figure("veloity time scaling profile")
-plot_vel = plt.plot(np.linspace(0, t, plot_index+1), vel_plot_data[0:plot_index+1])
-plot_path = plt.hlines(max_vel, 0, t, 'r', '--')
+plot_vel = plt.plot(np.linspace(delta_s, s-delta_s, plot_index), vel_plot_data[0:plot_index])
+plot_vel = plt.hlines(pp.max_vel, 0, 1, 'r')
+plot_vel = plt.vlines(pp.s_1, 0, pp.max_vel+20, 'g', '--')
+plot_vel = plt.vlines(1-pp.s_1, 0, pp.max_vel+20, 'g', '--')
+
+# time taken
+plot_delta_t = plt.figure("delta t")
+plot_delta_t = plt.plot(np.linspace(0, s, plot_index), delta_t_plot_data[0:plot_index],'.')
+plot_delta_t = plt.vlines(1, 0, max(delta_t_plot_data)*1.1, 'k', '--')
+plot_delta_t = plt.vlines(0, 0, max(delta_t_plot_data)*1.1, 'k', '--')
+plot_delta_t = plt.vlines(pp.s_1, 0, max(delta_t_plot_data)*1.1, 'g', '--')
+plot_delta_t = plt.vlines(1-pp.s_1, 0, max(delta_t_plot_data)*1.1, 'g', '--')
 
 
 plt.show()
