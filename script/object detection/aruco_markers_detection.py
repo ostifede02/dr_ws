cf9f950daa9586055997be6aadd1782d05655cbb
ostifede02 <ostifederico02@gmail.com>
2023-11-08 18:16:20 -0500

    aruco test

--- script/object detection/aruco_markers_detection.py
+++ script/object detection/aruco_markers_detection.py
@@ -1,28 +1,54 @@
-import cv2 
-import cv2.aruco as aruco
+import cv2
+import numpy as np
 
 
-vid = cv2.VideoCapture(0) 
+vid = cv2.VideoCapture("http://10.248.24.172:4747/video/400x400")
 
-while(True): 
-	
-	ret, frame = vid.read() 
-	aruco_dict = aruco.Dictionary(aruco.DICT_6X6_250)
-	corners, ids, rejected = aruco.detectMarkers(frame, aruco_dict)
-	image_with_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+# create the dictionary for markers type
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 
-	for i in range(len(ids)):
-		marker_id = ids[i][0]
-		marker_corners = corners[i][0]
-		print(f"marker_corners: {marker_corners}\tid: {marker_id}")
+intrinsic_camera = np.array(((933.15867, 0, 657.59),(0,933.1586, 400.36993),(0,0,1)))
+distortion = np.array((-0.43948,0.18514,0,0))
 
-	cv2.imshow('frame', frame) 
-	
 
-	if cv2.waitKey(1) & 0xFF == ord('q'): 
-		break
 
-# After the loop release the cap object 
-vid.release() 
-# Destroy all the windows 
-cv2.destroyAllWindows() 
+def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),cv2.BORDER_DEFAULT)
+    corners, marker_ids, rejected = cv2.aruco.detectMarkers(blur, dictionary)
+        
+    if len(corners) > 0:
+        for i in range(0, len(marker_ids)):
+           
+            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
+                                                                       distortion_coefficients)
+            
+            cv2.aruco.drawDetectedMarkers(frame, corners) 
+
+            cv2.drawFrameAxes(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  
+
+    return frame
+
+
+
+def main():
+
+    while True:
+        ret, frame = vid.read()
+
+        frame = pose_estimation(frame, dictionary, intrinsic_camera, distortion)
+
+        cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+
+
+    vid.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+
