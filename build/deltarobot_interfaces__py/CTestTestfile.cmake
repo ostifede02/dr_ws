f06c44d0e5b5b38b2a9568cdccfdb8dfc5233096
ostifede02 <ostifederico02@gmail.com>
2023-12-12 17:49:36 -0500

    .

--- build/deltarobot_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/ostifede02/F48A91EB8A91AB1E/Users/ostif/Documents/2dr_ws/build/deltarobot_interfaces__py
-# Build directory: /media/ostifede02/F48A91EB8A91AB1E/Users/ostif/Documents/2dr_ws/build/deltarobot_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
