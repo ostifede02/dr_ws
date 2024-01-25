e26ecef209f05dd2d357913c516956c73eb0d8a6
ostifede02 <ostifederico02@gmail.com>
2024-01-25 14:54:37 +0100

    .

--- src/microros/build/micro_ros_agent/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ostifede02/dr_ws/src/microros/src/uros/micro-ROS-Agent/micro_ros_agent
-# Build directory: /home/ostifede02/dr_ws/src/microros/build/micro_ros_agent
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/microros/build/micro_ros_agent/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ostifede02/dr_ws/src/microros/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
